a66f4241fe8e070b3a94741d11f0b440601c0cca
Павел Судаков <pashok6798@yandex.ru>
2023-04-12 10:57:55 +0300

    Обновил README.md и переписал функцию getWordsFromString

--- app/src/main/java/com/iate_is_b_19z/laboratory_1/MainActivity.kt
+++ app/src/main/java/com/iate_is_b_19z/laboratory_1/MainActivity.kt
@@ -6,26 +6,12 @@ import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ListView
 
-fun getWordsFromString() : List<String>{
-    val exampleString = "Одним из наставников Владимира Арсеньева в походах по горно-таёжным дебрям Дальнего Востока стал нанайский охотник Дерсу Узала. Никогда и нигде не учившийся, не умевший ни читать, ни писать, он обладал самым настоящим экологическим сознанием. Оно было впитано им сызмальства. Дерсу был не пришельцем извне, уссурийская тайга была его миром и домом. Всё сущее он называл «люди». Камни, ветер, река, огонь – всё это было для него «люди», не говоря уж о животных и растениях. Всё жило своей сокровенной жизнью, составляя удивительную таёжную симфонию. Не зная таких понятий, как экологическое равновесие или биологическая система, Дерсу, конечно же, хорошо представлял, что это такое. Когда Арсеньев, задумавшись о чём-то, бросил в костёр кусочек мяса, Дерсу выхватил его из огня и отчитал «капитана»: разве можно сжигать пищу? Уйдём отсюда мы – придут другие люди. Не енот – так барсук, не белка – так мышь или муравей…"
-
-    val regexSentence = Regex("[^A-Za-zА-Яа-я\\d ]")
-    val regexRemoveEmptyStrs = Regex("\n\\s*\n")
-
-    return regexSentence.replace(exampleString, "").replace(regexRemoveEmptyStrs, "").split(' ').distinct()
+fun getWordsFromString(myString: String) : List<String>{
+    return myString.replace("\\p{Punct}".toRegex(), "").split(" +".toRegex()).toList().distinct()
 }
 
-fun getWordsWithFirstUpperCase() : List<String> {
-    var res = getWordsFromString().toMutableList()
-
-    var i = 0
-
-    while(i < res.size) {
-        res[i] = res[i].replaceFirstChar(Char::uppercaseChar)
-        i++
-    }
-
-    return res.toList()
+fun getWordsWithFirstUpperCase(word: String) : String {
+    return word.replaceFirstChar(Char::uppercaseChar)
 }
 
 class MainActivity : AppCompatActivity() {
@@ -39,7 +25,8 @@ class MainActivity : AppCompatActivity() {
 
         btn.setOnClickListener {
             //val strs = getWordsFromString()
-            val strs = getWordsWithFirstUpperCase()
+            val exampleString = "Одним из наставников Владимира Арсеньева в походах по горно-таёжным дебрям Дальнего Востока стал нанайский охотник Дерсу Узала. Никогда и нигде не учившийся, не умевший ни читать, ни писать, он обладал самым настоящим экологическим сознанием. Оно было впитано им сызмальства. Дерсу был не пришельцем извне, уссурийская тайга была его миром и домом. Всё сущее он называл «люди». Камни, ветер, река, огонь – всё это было для него «люди», не говоря уж о животных и растениях. Всё жило своей сокровенной жизнью, составляя удивительную таёжную симфонию. Не зная таких понятий, как экологическое равновесие или биологическая система, Дерсу, конечно же, хорошо представлял, что это такое. Когда Арсеньев, задумавшись о чём-то, бросил в костёр кусочек мяса, Дерсу выхватил его из огня и отчитал «капитана»: разве можно сжигать пищу? Уйдём отсюда мы – придут другие люди. Не енот – так барсук, не белка – так мышь или муравей…"
+            val strs = getWordsFromString(exampleString)
 
             val arrayAdapter : ArrayAdapter<String> = ArrayAdapter(this, android.R.layout.simple_list_item_1, strs)
             listView.adapter = arrayAdapter
